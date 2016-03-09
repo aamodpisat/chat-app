1635a1d0b30dedaf77a6b7b0e280cbe61daa5e20
Aamod Pisat <aamod.pisat@raweng.com>
2016-03-09 15:25:05 +0530

    Index.html

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var i= 0;
+
+app.get('/',function(req,res){
+		 res.sendFile(__dirname + '/index.html');
+})
 io.on('connection', function(socket){
   console.log('App connected');
 	// id= id+1;
